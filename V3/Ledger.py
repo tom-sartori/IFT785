a5f468a5c614b9fca7dffb6d4765379cc3bc71a6
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2024-03-18 00:11:29 -0400

    Update Ledger public key type.

--- V3/Ledger.py
+++ V3/Ledger.py
@@ -7,12 +7,12 @@ from V3.fake_crypto import PublicKey
 class Ledger:
 
     def __init__(self):
-        self._accounts: dict[PublicKey, Account] = {}  # PublicKey -> Account.
+        self._accounts: dict[str, Account] = {}  # PublicKey.key -> Account.
 
     def __str__(self):
         result = 'Ledger contains the following accounts: \n'
-        for public_key, account in self._accounts.items():
-            result += f'- {public_key.owner}\n'
+        for account in self._accounts.values():
+            result += f'- {account.public_key.owner}\n'
             result += indent(account.__str__(), '\t')
 
         return result
@@ -24,9 +24,15 @@ class Ledger:
         if not self.verify():  # Verify the ledger before adding a new account.
             raise Exception('Error: Ledger verification failed. Can not add account. ')
 
-        self._accounts[account.public_key] = account
+        self._accounts[account.public_key.key] = account
 
-    def get_account(self, public_key: PublicKey) -> Account:
+    def get_account(self, public_key: PublicKey or str) -> Account:
+        if isinstance(public_key, PublicKey):
+            public_key = public_key.key
+
+        if public_key not in self._accounts:
+            raise Exception('Error: Account not found. ')
+
         return self._accounts[public_key]
 
     def verify(self) -> bool:
