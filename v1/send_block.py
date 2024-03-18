97217400f934464386347e5dbf95657e858b2018
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2024-03-18 10:58:41 -0400

    Revert "Created builder pattern for block and implement open_block, send_bloc…"

--- v1/send_block.py
+++
@@ -1,6 +0,0 @@
-from generic_block import GenericBlock
-
-class SendBlock(GenericBlock):
-    def __init__(self, data, previous_hash):
-        super().__init__(data, previous_hash)
-        self._data["type"] = "Generic Block"
