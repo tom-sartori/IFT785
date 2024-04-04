e794c056552399fbd766a21daae3045a5abe9946
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2024-04-04 14:47:59 -0400

    On fly init method creation.

--- test.py
+++ test.py
@@ -1,16 +1,11 @@
 import json
+from array import array
 
 from Account import Account
 from Block import Block
 from Ledger import Ledger
 from dsl.Verification import Verification
 from fake_crypto import generate_keys
-
-
-def init(self, previous_block, a, b):
-    Block.__init__(self, previous_block)
-    self.add_data('a', a)
-    self.add_data('b', b)
 
 
 def find_verification_method(method_name: str) -> callable:
@@ -30,13 +25,26 @@ if __name__ == '__main__':
 
     dsl = json.loads(open('dsl/dsl.json', 'r').read())
     block_str = dsl['blocks']['OpenNanocoin']
+    print(block_str['attributes'])
+
+    string = f"def init(self, previous_block, {', '.join(block_str['attributes'])}):"
+    string += """
+    Block.__init__(self, previous_block)
+
+    my_dict = {k: v for k, v in locals().items() if isinstance(v, int) or isinstance(v, str)}
+    for key, value in my_dict.items():
+        self.add_data(key, value)
+    """
+    compiled = compile(string, '', 'exec')
+    eval(compiled)
 
     SubBlock = type('SubBlock', (Block,), {
         '__init__': init,
         'data': block_str['data']
     })
 
+    # block = SubBlock(genesis_account.head, {'a': 5, 'b': 5})
     block = SubBlock(genesis_account.head, 5, 5)
     genesis_account.add_block(block)
 
-    print(genesis_account)
+    # print(genesis_account)
