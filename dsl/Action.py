725a24ad6e6c3bec4c9dbc883c89fe7b03904085
tom-sartori <60385058+tom-sartori@users.noreply.github.com>
2024-04-11 03:28:13 -0400

    Actions for receive bloc v1.

--- dsl/Action.py
+++ dsl/Action.py
@@ -24,10 +24,20 @@ def assign_balance_when_opening(block: 'Block', account: str or None) -> None:
         block.data['balance'] = 0
 
 
-def set_balance(block: 'Block', open_hash: str) -> None:
-    open_block: 'Block' = Ledger().get_block(open_hash)
-    account: 'Account' = Ledger().get_account(block.account_public_key)
-    balance = account.get_balance(open_block.data['unit'])
+def set_balance(block: 'Block', block_hash: str) -> None:
+    retrieved_block: 'Block' = Ledger().get_block(block_hash)
+
+    if 'unit' in retrieved_block.data:
+        # The block is an open block.
+        unit = retrieved_block.data['unit']
+    elif 'open_hash' in retrieved_block.data:
+        # The block is a send block.
+        open_block = Ledger().get_block(retrieved_block.data['open_hash'])
+        unit = open_block.data['unit']
+    else:
+        raise ValueError('The block is not an open or send block. Unable to set the balance.')
+
+    balance = Ledger().get_account(block.account_public_key).get_balance(unit)
     block.data['balance'] = balance
 
 
@@ -53,7 +63,19 @@ def increase_balance(block: 'Block', amount: int or float) -> None:
     block.data['balance'] = block.data['balance'] + amount
 
 
+def set_amount(block: 'Block', send_hash: str) -> None:
+    send_block: 'Block' = Ledger().get_block(send_hash)
+    block.data['amount'] = send_block.data['amount']
+
+
+def set_open_hash(block: 'Block', other_block_hash: str) -> None:
+    other_block: 'Block' = Ledger().get_block(other_block_hash)
+    block.data['open_hash'] = other_block.data['open_hash']
+
+
 Action().__add__(assign_balance_when_opening)
 Action().__add__(set_balance)
 Action().__add__(decrease_balance)
 Action().__add__(increase_balance)
+Action().__add__(set_amount)
+Action().__add__(set_open_hash)
