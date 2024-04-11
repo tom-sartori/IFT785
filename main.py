0f3d9718b7e0288322ceca91bc48567ed316ae77
nakeva <nanakevin24@gmail.com>
2024-04-10 22:56:14 -0400

    Update GUI...

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from BlockTypeRegister import BlockTypeRegister
 from Dsl import Dsl
 from Ledger import Ledger
 from fake_crypto import generate_keys
-from to_generate.OpenBlock import OpenBlock
 
 if __name__ == '__main__':
     dsl: Dsl = Dsl(dsl_file_name='dsl/dsl.json')
-    # BlockTypeRegister().add_block_types(dsl.blocks)
-    # print(BlockTypeRegister())
+    BlockTypeRegister().add_block_types(dsl.blocks)
+    print(BlockTypeRegister())
 
     genesis_account: Account = Account(*generate_keys('Genesis'))
     Ledger().add_account(genesis_account)
@@ -16,23 +15,12 @@ if __name__ == '__main__':
     second_account: Account = Account(*generate_keys('Second'))
     Ledger().add_account(second_account)
 
-    print("Genesis account", genesis_account.head)
+    open_nanocoin = BlockTypeRegister()['OpenNanocoin'](genesis_account.head)
+    genesis_account.add_block(open_nanocoin)
 
-    open_block_genesis = OpenBlock(
-        previous_block=genesis_account.head,
-        unit='nano_coin',
-        balance=1000,
-        account=genesis_account.public_key.key
+    genesis_account.add_block(
+        # 'previous_block', 'receiver', 'amount', and 'open_hash'
+        BlockTypeRegister()['Send'](genesis_account.head, second_account.public_key.key, 50, open_nanocoin.hash)
     )
 
-    genesis_account.add_block(open_block_genesis)
-
-    # open_nanocoin = BlockTypeRegister()['OpenNanocoin'](genesis_account.head)
-    # genesis_account.add_block(open_nanocoin)
-
-    # genesis_account.add_block(
-    #     # 'previous_block', 'receiver', 'amount', and 'open_hash'
-    #     BlockTypeRegister()['Send'](genesis_account.head, second_account.public_key.key, 50, open_nanocoin.hash)
-    # )
-
-    # print(Ledger())
+    print(Ledger())
--- user_interface_main.py
+++ user_interface_main.py
@@ -1,12 +1,24 @@
+"""
+        Creation of accounts and blocks from the graphical interface.
+
+        account creation syntax: create_account <account_name>
+        syntax for creating a block: create_block <owner_public_key> <receiver_public_key> <bloc_type> <unit> <balance> <minimal_balance>
+        receiver_public_key is the public key of the receiver account, and it's optional.
+
+        We must create an account before creating blocks
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import scrolledtext
 from tkinter import messagebox
 
 from Account import Account
+from BlockTypeRegister import BlockTypeRegister
+from Dsl import Dsl
 from Ledger import Ledger
 from fake_crypto import generate_keys
-from to_generate.OpenBlock import OpenBlock
+import json
 
 
 def create_account(name):
@@ -20,24 +32,50 @@ def create_account(name):
         update_ledger_display()
 
 
-def create_block(public_key, bloc_type, balance):
+def create_block(owner_public_key, receiver_public_key, block_type, unit, balance, minimal_balance):
+    file_path = 'dsl/dsl.json'
+
     print("Building block...")
-    account = Ledger().get_account(public_key)
+    account = Ledger().get_account(owner_public_key)
+    receiver_account = Ledger().get_account(receiver_public_key)
     print(account.public_key.key)
     if account is None:
         messagebox.showinfo("Error", "Account Not Found")
         raise Exception("Account Not Found")
     else:
-        # Test bloc creation
-        bloc = OpenBlock(
-            previous_block=account.head,
-            unit='nano_coin',
-            balance=balance,
-            account=account.public_key.key
-        )
+        update_json_dsl_file(file_path, block_type, unit, balance, minimal_balance)
+        dsl: Dsl = Dsl(dsl_file_name='dsl/dsl.json')
+        BlockTypeRegister().add_block_types(dsl.blocks)
+        print(BlockTypeRegister())
 
+        bloc = BlockTypeRegister()['OpenNanocoin'](account.head)
         account.add_block(bloc)
+        if receiver_account is not None:
+            account.add_block(
+                BlockTypeRegister()['Send'](account.head, receiver_account.public_key.key, 25, bloc.hash)
+            )
+
         update_ledger_display()
+
+
+def update_json_dsl_file(file_path, block_type, unit, balance, minimal_balance):
+    with open(file_path, 'r') as file:
+        data = json.load(file)
+    # Modify attributes according to provided variables
+    if 'blocks' in data and 'OpenNanocoin' in data['blocks']:
+        # Rename 'OpenNanocoin' key to 'NewBlockType'
+        data['blocks'][block_type] = data['blocks'].pop('OpenNanocoin')
+
+        # Edit 'unit', 'balance' and 'minimal_balance' attributes
+        if 'data' in data['blocks'][block_type]:
+            data_block = data['blocks'][block_type]['data']
+            data_block['unit'] = unit
+            data_block['balance'] = int(balance)
+            data_block['minimal_balance'] = int(minimal_balance)
+
+    # Write changes back to JSON file
+    with open(file_path, 'w') as file:
+        json.dump(data, file, indent=4)
 
 
 def update_ledger_display():
@@ -51,6 +89,9 @@ def update_ledger_display():
     for account in accounts:
         print(account.public_key.key)
         ledger_view.insert('', tk.END, values=(account.public_key.owner, account.public_key.key, len(account._chain)))
+
+    print(Ledger())
+
 
 def is_account_already_exist(name):
     accounts = Ledger()._accounts.values()
@@ -68,15 +109,15 @@ def process_command():
         create_account(name)
         messagebox.showinfo("Success", f"Account '{name}' successfully created!")
     elif command.startswith("create_block"):
-        _, public_key, bloc_type, data = command.split()
-        create_block(public_key, bloc_type, data)
-        messagebox.showinfo("Success", f"Bloc successfully created with data: {data}")
+        _, public_key, receiver_public_key, bloc_type, unit, balance, minimal_balance = command.split()
+        create_block(public_key, receiver_public_key, bloc_type, unit, balance, minimal_balance)
+        messagebox.showinfo("Success", "Bloc successfully created")
     else:
         messagebox.showerror("Error", "Invalid command command.")
     command_entry.delete("1.0", tk.END)
 
 
-# Configuration de l'interface graphique
+# Configuring the GUI
 root = tk.Tk()
 root.title("Nano Blockchain Command Interface")
 
@@ -92,7 +133,7 @@ submit_button.pack()
 ledger_label = tk.Label(root, text="Ledger (Accounts):")
 ledger_label.pack()
 
-# Configuration de l'affichage du Ledger
+# Configuring the Ledger display
 ledger_view = ttk.Treeview(root, columns=('Name', 'PublicKey', 'Blocks'), show='headings')
 ledger_view.heading('Name', text='Account owner')
 ledger_view.heading('PublicKey', text='Public key')
