29b89e5f964ab7ab3069d9317a1bcdbec427e27c
johan237 <johantchassem@gmail.com>
2024-04-22 20:47:03 -0400

    update ds

--- src/main/dsl/Dsl.py
+++ src/main/dsl/Dsl.py
@@ -52,56 +52,64 @@ class Dsl:
         :return: bool True if the block definition is verified, False otherwise.
         """
 
-        # block_documentation = self.documentation['blocks']['Open'] \
-        #     if self._is_open__block(block_definition) else self.documentation['blocks']['GenericBloc']
-        #
-        # if block_documentation is None:
-        #     print(f'Error: Block type {block_definition["block_type"]} has no documentation. ')
-        #     return False
-        #
+        block_documentation = self.documentation['blocks']['Open'] \
+            if self._is_open__block(block_definition) else self.documentation['blocks']['GenericBloc']
+        
+        if block_documentation is None:
+            print(f'Error: Block type has no documentation. ')
+            return False
+        
+        is_divisible = block_definition["is_divisible"] if "is_divisible" in block_definition.keys() else True
+        print("is_divisible", is_divisible)
+        if "transaction_fee" in block_definition.keys() and not is_divisible :
+                print(f'Error: Block {block_definition["unit"]} Cannot have transaction_fee and not divisible (is_divisible: false). Note that by default is_divisible is true, so you have to set it explicitly to false otherwise. ')
+                return False
+        
+        # If the attribute is not divisible, it must be an int
+        is_divisible_type = (int, float)
+        if not is_divisible :
+            is_divisible_type = int
+                
         # # TODO: For block_documentation.keys().
         # # TODO: Can't have is_divisible and transaction_fee.
-        # for key in block_definition.keys():
-        #     if key not in block_documentation.keys():
-        #         print(f'Error: Block type {block_definition["block_type"]} has unknown key {key}. ')
-        #         return False
-        #
-        #     documentation_attributes = block_documentation[key]
-        #     key_is_required = documentation_attributes['required']
-        #     if key_is_required and key not in block_definition.keys():
-        #         print(f'Error: Block type {block_definition["block_type"]} has missing key {key}. ')
-        #         return False
-        #
-        #     # Par defaut attribute_type est secable donc int or float sont vrai
-        #     attribute_type = eval(documentation_attributes['type'])
-        #     if documentation_attributes["type"] == 'int or float':
-        #         attribute_type = (int, float)
-        #
-        #     # If the attribute is not divisible, it must be an int
-        #     if "is_divisible" in block_definition.keys() and block_definition["is_divisible"] is False :
-        #         attribute_type = int
-        #
-        #     if not isinstance(block_definition[key], attribute_type) and not isinstance(block_definition[key], type(None)):
-        #         print(type(block_definition[key]))
-        #         print(attribute_type)
-        #         # print(documentation_attributes["type"])
-        #         print(f'Error: Block type {block_definition["block_type"]} has wrong type for key {key} with value {block_definition[key]}. ')
-        #         return False
+        for key in block_definition.keys():
+            # if key not in block_documentation.keys():
+            #     print(f'Error: Block {block_definition} has unknown key {key}. ')
+            #     return False
+        
+            documentation_attributes: dict = block_documentation[key]
+            key_is_required: bool = documentation_attributes['required']
+            
+            if key_is_required and key not in block_definition.keys():
+                print(f'Error: Block type {block_definition} has missing key {key}. ')
+                return False
+        
+            # Par defaut attribute_type est secable donc int or float sont vrai
+            attribute_type = eval(documentation_attributes['type'])
+            if documentation_attributes["type"] == 'int or float':
+                attribute_type = is_divisible_type
+            
+            if not isinstance(block_definition[key], attribute_type) and not isinstance(block_definition[key], type(None)):
+                print(type(block_definition[key]))
+                print(attribute_type)
+                # print(documentation_attributes["type"])
+                print(f'Error: Block type {block_definition} has wrong type for key {key} with value {block_definition[key]}. ')
+                return False
 
         # if block_documentation.keys() != block_definition.keys():
-        #     print(f'Error: Block type {block_definition["block_type"]} has different keys than the documentation. ')
+        #     print(f'Error: Block type {block_definition} has different keys than the documentation. ')
         #     return False
-        #
+        
         # for key, value in block_documentation.items():
         #     attribute = block_definition[key]
-        #
+        
         #     # Check type.
         #     if not isinstance(attribute, eval(value['type'])):
-        #         print(f'Error: Block type {block_definition["block_type"]} has wrong type for key {key}. ')
+        #         print(f'Error: Block type {block_definition} has wrong type for key {key}. ')
         #         return False
-        #
+        
         #     if 'value' in value.keys() and value['value'] != attribute:
-        #         print(f'Error: Block type {block_definition["block_type"]} has wrong value for key {key}. ')
+        #         print(f'Error: Block type {block_definition} has wrong value for key {key}. ')
         #         return False
 
         # return True
