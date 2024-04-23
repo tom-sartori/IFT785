b5555f2eb72d728a74f48d72048b7c786843e1b7
johan237 <johantchassem@gmail.com>
2024-04-23 09:08:48 -0400

    test folder init

--- test/ledger/account/test_account.py
+++ test/ledger/account/test_account.py
@@ -1,25 +1,25 @@
-# import unittest
+import unittest
 
-# from main.ledger.account.Account import Account
-# from main.utils.fake_crypto import generate_keys
+from main.ledger.account.Account import Account
+from main.utils.fake_crypto import generate_keys
 
 
-# class TestAccount(unittest.TestCase):
+class TestAccount(unittest.TestCase):
 
-#     @classmethod
-#     def setUpClass(cls):
-#         (cls.genesisPrivateKey, cls.genesisPublicKey) = generate_keys('Genesis')
+    @classmethod
+    def setUpClass(cls):
+        (cls.genesisPrivateKey, cls.genesisPublicKey) = generate_keys('Genesis')
 
-#     def setUp(self):
-#         self.account = Account(self.genesisPrivateKey, self.genesisPublicKey)
+    def setUp(self):
+        self.account = Account(self.genesisPrivateKey, self.genesisPublicKey)
 
-#     def tearDown(self):
-#         self.account = None
+    def tearDown(self):
+        self.account = None
 
-#     @classmethod
-#     def tearDownClass(cls):
-#         cls.genesisPrivateKey = None
-#         cls.genesisPublicKey = None
+    @classmethod
+    def tearDownClass(cls):
+        cls.genesisPrivateKey = None
+        cls.genesisPublicKey = None
 
-#     def test_publicKey(self):
-#         self.assertEqual(self.account.public_key, self.genesisPublicKey)
+    def test_publicKey(self):
+        self.assertEqual(self.account.public_key, self.genesisPublicKey)
